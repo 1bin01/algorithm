6c706cf5b7d494e6a7a8c0795d4cba060196c070
1bin <wonbin0504@naver.com>
2021-10-13 12:25:56 +0900

    컨벡스헐 트릭 O(n)

--- Dp_Optimization/Convex_Hull_Trick.cpp
+++ Dp_Optimization/Convex_Hull_Trick.cpp
@@ -1,28 +1,19 @@
-#include <iostream>
-#include <algorithm>
+// 기울기 단조 감소, x값 단조 증가
+// 최솟값 구하기 O(n)
 
-using namespace std;
+ll m[NMAX], p[NMAX];
+int sz, ii;
 
-#define all(v) v.begin(), v.end()
-typedef long long ll;
-const ll NMAX = 1e5 + 5;
-ll n;
-ll A[NMAX], B[NMAX], m[NMAX], p[NMAX], sz, ii;
-
-// BOJ 13263 나무 자르기
-// CHT (기울기 단조 감소, x 단조 증가) - 최솟값 구하기 O(n)
-
-
-double cross(ll a, ll b) { return 1.0 * (p[b] - p[a]) / (m[a] - m[b]);  }
+double cross(int a, int b) { return 1.0 *(p[b] - p[a]) / (m[a] - m[b]); }
 
 void insert(ll mm, ll pp) {
 	m[sz] = mm;
 	p[sz] = pp;
 
-	while (sz > 1 && cross(sz - 1, sz - 2) >= cross(sz - 1, sz)) {
+	while (sz > 1 && cross(sz - 2, sz - 1) >= cross(sz - 1, sz)) {
 		m[sz - 1] = m[sz];
 		p[sz - 1] = p[sz];
-		sz--;
+		if (--sz == ii) ii--;
 	}
 	sz++;
 	return;
@@ -30,20 +21,5 @@ void insert(ll mm, ll pp) {
 
 ll go(ll x) {
 	while (ii + 1 < sz && cross(ii, ii + 1) <= x) ii++;
-	return x * m[ii] + p[ii];
-}
-
-int main(void) {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-	cin >> n;
-	for (int i = 1; i <= n; i++) cin >> A[i];
-	for (int i = 1; i <= n; i++) cin >> B[i];
-	insert(B[1], 0);
-	for (int i = 2; i <= n; i++) {
-		p[i] = go(A[i]);
-		insert(B[i], p[i]);
-	}
-	cout << p[n] << '\n';
-	return 0;
+	return m[ii] * x + p[ii];
 }
