bfc1506620a9c9b6c6eee5491a6948efc858f612
1bin <wonbin0504@naver.com>
2021-12-02 16:29:13 +0900

    Suffix Array O(nlogn^2)

---
+++ string/Suffix_array_nlogn2.cpp
@@ -0,0 +1,20 @@
+int n, sa[NMAX], g[NMAX], tmp[NMAX];
+string s;
+
+for (int i = 0; i < n; i++) {
+		g[i] = s[i] - 'a' + 1;
+		sa[i] = i;
+	}
+
+	for (int d = 1; d < n; d *= 2) {
+		auto cmp = [&](int i, int j) {
+			if (g[i] == g[j]) return g[i + d] < g[j + d];
+			else return g[i] < g[j];
+		};
+		sort(sa, sa + n, cmp);
+
+		tmp[sa[0]] = 1;
+		for (int i = 1; i < n; i++)
+			tmp[sa[i]] = tmp[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
+		for (int i = 0; i < n; i++) g[i] = tmp[i];
+	}
