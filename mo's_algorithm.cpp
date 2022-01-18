9c42346a780ddb67c0782f50420af928e7a7467c
1bin <wonbin0504@naver.com>
2022-01-18 19:21:21 +0900

    모스 알고리즘

---
+++ mo's_algorithm.cpp
@@ -0,0 +1,36 @@
+const int NMAX = 1e5 + 5;
+const int b = 350;
+int n, a[NMAX], cnt[NMAX], Q, ret, ans[NMAX];
+
+struct query {
+	int i, l, r;
+	bool operator <(const query& x) const {
+		if (l / b == x.l / b) return r < x.r;
+		return l / b < x.l / b;
+	}
+} q[NMAX];
+
+void f(int c, int ad) {
+	if (ad) {
+		if (cnt[c]++ == 0) ret++;
+	}
+	else {
+		if (cnt[c]-- == 1) ret--;
+	}
+	return;
+}
+
+for (int i = 0; i < Q; i++) {
+		cin >> q[i].l >> q[i].r;
+		q[i].i = i;
+	}
+	sort(q, q + Q);
+	int l = 1, r = 0;
+	for (int i = 0; i < Q; i++) {
+		while (l > q[i].l) f(a[--l], 1);
+		while (l < q[i].l) f(a[l++], 0);
+		while (r < q[i].r) f(a[++r], 1);
+		while (r > q[i].r) f(a[r--], 0);
+		ans[q[i].i] = ret;
+	}
+  for (int i = 0; i < Q; i++) cout << ans[i] << '\n';
