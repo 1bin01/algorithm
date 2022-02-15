894bfd239732ad1057d944fec0aa5f129e609f6d
1bin <wonbin0504@naver.com>
2022-02-15 16:05:14 +0900

    2-SAT

--- graph/2_SAT.cpp
+++ graph/2_SAT.cpp
@@ -1,69 +1,29 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <stack>
-#include <cstring>
-
-using namespace std;
-
-const int NMAX = 2e4 + 5;
-int N, M, a, b, flag, dfsn[NMAX], cnt, fin[NMAX], sz, sccnum[NMAX];
+// 배열 크기는 2배로
+int n, m;
+int dfsn[NMAX], fin[NMAX], sn[NMAX], sz, t;
 vector<int> adj[NMAX];
-vector<vector<int>> scc;
-
-// BOJ 11281 2-SAT -4
-// 타잔 알고리즘을 이용한 2-SAT 구현
+stack<int> st;
 
 int Not(int x) {
-	if (x <= N) return x + N;
-	return x - N;
+	return x <= n ? x + n : x - n;
 }
 
-stack<int> st;
 int dfs(int now) {
-	dfsn[now] = ++cnt;
+	int ret = dfsn[now] = ++t;
 	st.emplace(now);
 
-	int ret = dfsn[now];
-	for (int nxt : adj[now]) {
-		if (!dfsn[nxt]) ret = min(ret, dfs(nxt));
-		else if (!fin[nxt]) ret = min(ret, dfsn[nxt]);
+	for(int nx : adj[now]) {
+		if(!dfsn[nx]) ret = min(ret, dfs(nx));
+		else if(!fin[nx]) ret = min(ret, dfsn[nx]);
 	}
 
-	if (ret == dfsn[now]) {
-		vector<int> cur;
-		while (1) {
+	if(ret == dfsn[now]) {
+		while(1) {
 			int x = st.top(); st.pop();
-			cur.emplace_back(x);
-			fin[x] = 1;
-			sccnum[x] = sz;
-			if (x == now) break;
+			sn[x] = sz; fin[x]= 1;
+			if(x == now) break;
 		}
-		scc.emplace_back(cur); sz++;
+		sz++;
 	}
 	return ret;
-}
-
-int main(void) {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-	cin >> N >> M;
-	while (M--) {
-		cin >> a >> b;
-		if (a < 0) a = -a + N;
-		if (b < 0) b = -b + N;
-		adj[Not(a)].emplace_back(b);
-		adj[Not(b)].emplace_back(a);
-	}
-
-	for (int i = 1; i <= 2 * N; i++)
-		if (!dfsn[i]) dfs(i);
-	flag = 1;
-	for (int i = 1; i <= N; i++)
-		if (sccnum[i] == sccnum[i + N]) flag = 0;
-
-	cout << flag << '\n';
-	if (!flag) return 0;
-	for (int i = 1; i <= N; i++) cout << (sccnum[i] < sccnum[Not(i)]) << ' ';
-	return 0;
 }
