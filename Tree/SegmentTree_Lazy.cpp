2096455aceb2842876fb7daa06fbf6e9df744d86
1bin <wonbin0504@naver.com>
2022-08-04 20:20:11 +0900

    lazy segment tree

--- Tree/SegmentTree_Lazy.cpp
+++ Tree/SegmentTree_Lazy.cpp
@@ -1,37 +1,36 @@
-void upd_lazy(int idx, int l, int r) {
-	if (!lazy[idx]) return;
-	seg[idx] += (r - l + 1) * lazy[idx];
-	if (l != r) {
-		lazy[idx * 2] += lazy[idx];
-		lazy[idx * 2 + 1] += lazy[idx];
-	}
-	lazy[idx] = 0;
-	return;
+const int NMAX = 1 << 18; // n * 2
+int n, seg[NMAX], lazy[NMAX];
+
+void upd_lazy(int idx, int l, int r){
+    if(!lazy[idx]) return;
+    seg[idx] += lazy[idx];
+    if(l != r){
+        lazy[idx * 2] += lazy[idx];
+        lazy[idx * 2 + 1] += lazy[idx];
+    }
+    lazy[idx] = 0;
+    return;
 }
 
-void update(int idx, int nl, int nr, int l, int r, ll val) {
-	upd_lazy(idx, nl, nr);
-	if (nl > r || nr < l) return;
-	if (nl >= l && nr <= r) {
-		seg[idx] += (nr - nl + 1) * val;
-		if (nl != nr) {
-			lazy[idx * 2] += val;
-			lazy[idx * 2 + 1] += val;
-		}
-		return;
-	}
-
-	int m = (nl + nr) / 2;
-	update(idx * 2, nl, m, l, r, val);
-	update(idx * 2 + 1, m + 1, nr, l, r, val);
-	seg[idx] = seg[idx * 2] + seg[idx * 2 + 1];
-	return;
+void upd(int idx, int nl, int nr, int l, int r, int v){
+    upd_lazy(idx, nl, nr);
+    if(nl > r || nr < l) return;
+    if(nl >= l && nr <= r){
+        lazy[idx] += v;
+        upd_lazy(idx, nl, nr);
+        return;
+    }
+    int m = (nl + nr) / 2;
+    upd(idx * 2, nl, m, l, r, v);
+    upd(idx * 2 + 1, m + 1, nr, l, r, v);
+    seg[idx] = seg[idx * 2] + seg[idx * 2 + 1];
+    return;
 }
 
-ll Sum(int idx, int nl, int nr, int l, int r) {
-	upd_lazy(idx, nl, nr);
-	if (nl > r || nr < l) return 0;
-	if (nl >= l && nr <= r) return seg[idx];
-	int m = (nl + nr) / 2;
-	return Sum(idx * 2, nl, m, l, r) + Sum(idx * 2 + 1, m + 1, nr, l, r);
+int qry(int idx, int nl, int nr, int l, int r){
+    upd_lazy(idx, nl, nr);
+    if(nl > r || nr < l) return 0;
+    if(nl >= l && nr <= r) return seg[idx];
+    int m = (nl + nr) / 2;
+    return qry(idx * 2, nl, m, l, r) + qry(idx * 2 + 1, m + 1, nr, l, r);
 }
