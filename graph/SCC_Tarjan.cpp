aa32fd059aeb1e15e218a88cc983cf7530a502d4
1bin <wonbin0504@naver.com>
2022-08-26 20:00:27 +0900

    SCC Tarjan

--- graph/SCC_Tarjan.cpp
+++ graph/SCC_Tarjan.cpp
@@ -1,60 +1,29 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <stack>
-
-using namespace std;
-
-const int NMAX = 1e4 + 5;
-int V, E, a, b, dfsn[NMAX], cnt, fin[NMAX], sz, sccnum[NMAX];
+int n, dfsn[NMAX], fin[NMAX], sn[NMAX], t, sz;
 vector<int> adj[NMAX];
-stack<int> st;
 vector<vector<int>> scc;
+stack<int> st;
 
-// BOJ 2150 Strongly Connected Component
-// SCC - 타잔 알고리즘
-
-int dfs(int now) {
-	dfsn[now] = ++cnt;
-	st.emplace(now);
-
-	int ret = dfsn[now];
-	for (int nxt : adj[now]) {
-		if (!dfsn[nxt]) ret = min(ret, dfs(nxt));
-		else if (!fin[nxt]) ret = min(ret, dfsn[nxt]);
-	}
-
-	if (ret == dfsn[now]) {
-		vector<int> cur;
-		while (1) {
-			int x = st.top(); st.pop();
-			cur.emplace_back(x);
-			fin[x] = 1;
-			sccnum[x] = sz;
-			if (x == now) break;
-		}
-		//sort(cur.begin(), cur.end());
-		scc.emplace_back(cur); sz++;
-	}
-	return ret;
-}
-
-int main(void) {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	
-	cin >> V >> E;
-	while (E--) {
-		cin >> a >> b;
-		adj[a].emplace_back(b);
-	}
-	for (int i = 1; i <= V; i++) 
-		if(!dfsn[i]) dfs(i);
-
-	sort(scc.begin(), scc.end());
-	cout << sz << '\n';
-	for (int i = 0; i < sz; i++) {
-		for (int x : scc[i]) cout << x << ' ';
-		cout << -1 << '\n';
-	}
-	return 0;
+int dfs(int x){
+    dfsn[x] = ++t;
+    st.emplace(x);
+    
+    int ret = dfsn[x];
+    for(int& nx : adj[x]){
+        if(!dfsn[nx]) ret = min(ret, dfs(nx));
+        else if(!fin[nx]) ret = min(ret, dfsn[nx]);
+    }
+    
+    if(ret == dfsn[x]){
+        vector<int> v;
+        while(1){
+            int y = st.top(); st.pop();
+            v.emplace_back(y);
+            fin[y] = 1;
+            sn[y] = sz;
+            if(y == x) break;
+        }
+        sort(all(v));
+        scc.emplace_back(v); sz++;
+    }
+    return ret;
 }
