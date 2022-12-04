bdd4c1122f3087a364524468af256cda0e009966
1bin <wonbin0504@naver.com>
2022-12-04 20:05:57 +0900

    Update 2_SAT.cpp

--- graph/2_SAT.cpp
+++ graph/2_SAT.cpp
@@ -8,22 +8,23 @@ int Not(int x) {
 	return x <= n ? x + n : x - n;
 }
 
-int dfs(int now) {
-	int ret = dfsn[now] = ++t;
-	st.emplace(now);
-
-	for(int nx : adj[now]) {
-		if(!dfsn[nx]) ret = min(ret, dfs(nx));
-		else if(!fin[nx]) ret = min(ret, dfsn[nx]);
-	}
-
-	if(ret == dfsn[now]) {
-		while(1) {
-			int x = st.top(); st.pop();
-			sn[x] = sz; fin[x]= 1;
-			if(x == now) break;
-		}
-		sz++;
-	}
-	return ret;
+stack<int> st;
+int dfs(int x){
+    int ret = dfsn[x] = ++t;
+    st.emplace(x);
+    
+    for(int& nx : adj[x]){
+        if(!dfsn[nx]) ret = min(ret, dfs(nx));
+        else if(!fin[nx]) ret = min(ret, dfsn[nx]);
+    }
+    
+    if(ret == dfsn[x]){
+        while(1){
+            int y = st.top(); st.pop();
+            sn[y] = sz; fin[y] = 1;
+            if(y == x) break;
+        }
+        sz++;
+    }
+    return ret;
 }
