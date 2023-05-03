c63736b40a4a41d8b71a13c6ac4ca87bea721a21
1bin <wonbin0504@naver.com>
2023-05-03 23:12:37 +0900

    Create BCC.cpp

---
+++ graph/BCC.cpp
@@ -0,0 +1,37 @@
+void dfs(int x, int p){
+    dfsn[x] = low[x] = ++t;
+    int f = 0, c = 0;
+    
+    for(int& nx : adj[x]){
+        if(nx == p) continue;
+        if(!dfsn[nx]) {
+            dfs(nx, x), c++;
+            low[x] = min(low[x], low[nx]);
+            if(low[nx] >= dfsn[x]) f = 1;
+            if(low[nx] > dfsn[x]) ans[1]++; // 단절선
+        }
+        else low[x] = min(low[x], dfsn[nx]);
+    }
+    if(f && (p || c > 1)) ans[0]++; // 단절점
+    return;
+}
+
+// BCC 
+void dfs2(int x, int p, int c){
+    vis[x] = 1;
+    for(int& nx : adj[x]){
+        if(nx == p) continue;
+        if(dfsn[nx] < dfsn[x]) cnt[c]++;
+        if(vis[nx]) continue;
+        
+        if(low[nx] >= dfsn[x]){
+            cnt[++sz]++;
+            dfs2(nx, x, sz);
+        }
+        else {
+            cnt[c]++;
+            dfs2(nx, x, c);
+        }
+    }    
+    return;
+}
