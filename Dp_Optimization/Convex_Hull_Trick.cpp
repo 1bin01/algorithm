aae94d5c72a843d6cdec87c5c48cad4934210c97
1bin <wonbin0504@naver.com>
2023-07-30 14:11:09 +0900

    ConvexHull Trick O(n)

--- Dp_Optimization/Convex_Hull_Trick.cpp
+++ Dp_Optimization/Convex_Hull_Trick.cpp
@@ -1,17 +1,15 @@
-// 기울기 단조 감소, x값 단조 증가
-// 최솟값 구하기 O(n)
-// 기울기가 같은 경우 조심!
+// 기울기, x좌표 단조 증가 O(n)
+// 최소값 구하기 upper bound convexhull
+// 기울기가 같은 경우 조심
 
-ll m[NMAX], p[NMAX];
-int sz, ii;
-
-long double cross(int a, int b) { return (long double) (p[b] - p[a]) / (m[a] - m[b]); }
+ll m[NMAX], p[NMAX], sz, ii;
+int bad(int a, int b, int c){return (p[b] - p[a]) * (m[b] - m[c]) >= (p[c] - p[b]) * (m[a] - m[b]);}
+ll get(int i, ll x){return m[i] * x + p[i];}
 
 void insert(ll mm, ll pp) {
 	m[sz] = mm;
 	p[sz] = pp;
-
-	while (sz > 1 && cross(sz - 2, sz - 1) >= cross(sz - 1, sz)) {
+	while(sz > 1 && bad(sz - 2, sz - 1, sz)){
 		m[sz - 1] = m[sz];
 		p[sz - 1] = p[sz];
 		if (--sz == ii) ii--;
@@ -20,7 +18,7 @@ void insert(ll mm, ll pp) {
 	return;
 }
 
-ll go(ll x) {
+ll f(ll x) {
 	while (ii + 1 < sz && cross(ii, ii + 1) <= x) ii++;
-	return m[ii] * x + p[ii];
+	return get(ii, x);
 }
