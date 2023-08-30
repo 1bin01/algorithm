2e4402f49dd3e1f9bd0ab63ee9ae8598fc38b797
1bin <wonbin0504@naver.com>
2023-08-30 22:40:47 +0900

    이중 해싱

--- string/hashing.cpp
+++ string/hashing.cpp
@@ -1,9 +1,15 @@
 // P(modular number) should be prime number
 const ll M1 = 1e9 + 7, M2 = 1e9 + 9;
 // generate base in random
-ll X1, X2;
-ll pfx[NMAX][2], pw[NMAX][2];
+ll n, X1, X2, H[NMAX][2], pw[NMAX][2], 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// 문자열 s[l, r]을 해싱하기
+pair<ll, ll> Hash(int l, int r){
+	ll y1 = ((H[l][0] - H[r + 1][0] * pw[r - l + 1][0]) % M1 + M1) % M1;
+	ll y2 = ((H[l][1] - H[r + 1][1] * pw[r - l + 1][1]) % M2 + M2) % M2;
+	return {y1, y2};
+}
 
 // 범위 (문자의 개수 + 1, M1 - 1)
 static uniform_int_distribution<int> f(300, M1 - 1);
@@ -15,10 +21,8 @@ for(int i = 1; i <= n; i++){
 	pw[i][1] = pw[i - 1][1] * X2 % M2;
 }
 
-// S = s[1] * X^1 + s[2] * X^2 ... + s[n] * X^n
-for(int i = 1; i <= n; i++){
-		pfx[i][0] = (pfx[i - 1][0] + s[i - 1] * pw[i][0]) % M1;
-		pfx[i][1] = (pfx[i - 1][1] + A[i - 1] * pw[i][1]) % M2;
+// H[i] = s[i] * X^0 + s[i + 1] * X^1 + ... + s[n - 1] * X^n-1-i
+for(int i = n - 1; i >= 0; i--) {
+	H[i][0] = (H[i + 1][0] * X1 + s[i]) % M1;
+	H[i][1] = (H[i + 1][1] * X2 + s[i]) % M2;
 }
-
-// a[i] * X^i + ... a[j] * X^j 구간 비교하기 -> 최고차항을 X^n으로 맞추고 비교
