006db1e76dee937244ca2f7980d29b34052a9de9
1bin <wonbin0504@naver.com>
2023-11-22 21:41:30 +0900

    pow function

---
+++ math/pow_function.cpp
@@ -0,0 +1,14 @@
+const ll mod = 1e9 + 7;
+
+// calculate x ^ k
+// time complexity : O(log(k))
+ll Pow(ll x, ll k) {		
+	ll ret = 1;
+	while (x) {
+		if (k & 1) {
+			ret *= x; ret %= mod;
+		}
+		x = x * x % mod; k >>= 1;
+	}
+	return ret;
+}
