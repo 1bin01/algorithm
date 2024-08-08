dc22ae80785516744476132cb1bfb74ea56cf2ae
1bin <wonbin0504@naver.com>
2024-08-08 13:50:31 +0900

    Update Lcp_array.cpp

--- string/Lcp_array.cpp
+++ string/Lcp_array.cpp
@@ -1,10 +1,15 @@
 int isa[NMAX], lcp[NMAX], l;
 string s;
 
-  s += '$';
-	for (int i = 0; i < n; i++) isa[sa[i]] = i;
-	for (int i = 0; i < n; i++, l = max(l - 1, 0)) {
-		if (!isa[i]) continue;
-		for (int j = sa[isa[i] - 1]; s[i + l] == s[j + l]; l++);
-		lcp[isa[i]] = l;
-	}
+void getLCP(string& s){
+    s += '$';
+    l = 0;
+
+    for(int i = 0; i < n; i++) isa[sa[i]] = i;
+    for(int i = 0; i < n; i++, l = max(l - 1, 0)){
+        if(!isa[i]) continue;
+        for(int j = sa[isa[i] - 1]; s[i + l] == s[j + l]; l++);
+        lcp[isa[i]] = l;
+    }
+    return;
+}
