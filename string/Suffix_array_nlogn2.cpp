87744a9a0e1e092661c3b7dd2cb484aad4ed13be
1bin <wonbin0504@naver.com>
2024-08-08 13:43:11 +0900

    Suffix_array nlogn^2

--- string/Suffix_array_nlogn2.cpp
+++ string/Suffix_array_nlogn2.cpp
@@ -2,21 +2,25 @@
 int n, sa[NMAX], g[NMAX], tmp[NMAX];
 string s;
 
-for (int i = 0; i < n; i++) {
-		g[i] = s[i] - 'a' + 1;
-		sa[i] = i;
-	}
+void getSA(string& s){
+    int n = s.size();
+    for(int i = 0; i < n; i++){
+        g[i] = s[i] - 'a' + 1;
+        sa[i] = i;
+    }
 
-	for (int d = 1; d < n; d *= 2) {
-		auto cmp = [&](int i, int j) {
-			if (g[i] == g[j]) return g[i + d] < g[j + d];
-			else return g[i] < g[j];
-		};
-		sort(sa, sa + n, cmp);
+    for(int d = 1; d < n; d *= 2){
+        auto cmp = [&](int i, int j){
+            if(g[i] == g[j]) return g[i + d] < g[j + d];
+            else return g[i] < g[j];
+        };
+        sort(sa, sa + n, cmp);
 
-		tmp[sa[0]] = 1;
-		for (int i = 1; i < n; i++)
-			tmp[sa[i]] = tmp[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
-		for (int i = 0; i < n; i++) g[i] = tmp[i];
-		if (g[sa[n - 1]] == n) break;
-	}
+        tmp[sa[0]] = 1;
+        for(int i = 1; i < n; i++)
+            tmp[sa[i]] = tmp[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
+        for(int i = 0; i < n; i++) g[i] = tmp[i];
+        if(g[sa[n - 1]] == n) break;
+    }
+    return;
+}
