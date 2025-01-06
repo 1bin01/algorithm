ec2cd8ad77a2f5ca409f074f75bf6da64d70f133
WONBIN CHO <wonbin0504@naver.com>
2025-01-06 18:00:19 +0900

    Update Persistent_Segment_Tree.cpp

--- tree/Persistent_Segment_Tree.cpp
+++ tree/Persistent_Segment_Tree.cpp
@@ -1,51 +1,53 @@
-onst int NMAX = 1e5 + 5;
-// SZ 크기 조절해주기
-const int SZ = 20 * NMAX;
-int rt[SZ], lc[SZ], rc[SZ], t;
-ll val[SZ];
+const int B = 1 << 18;
+namespace pst {
+    struct node {
+        ll v;
+        int l, r;
+    } nd[20 * B];
+    int root[B];
+    int t = 0;
 
-int init(int l, int r) {
-	int ix = ++t;
-	if(l == r) return ix;
-	int m = (l + r) >> 1;
-	lc[ix] = init(l, m);
-	rc[ix] = init(m + 1, r);
-	return ix;
+    int new_node(int x) {
+        nd[++t]={ x, 0, 0 };
+        return t;
+    }
+    int init(int l, int r) {
+        int ix = new_node(0);
+        if(l == r) return ix;
+        int m = l + r >> 1;
+        nd[ix].l = init(l, m);
+        nd[ix].r = init(m + 1, r);
+        return ix;
+    }
+    int update(int prev, int i, int l, int r, ll v) {
+        int ix = new_node(nd[prev].v + v);
+        if(l == r) return ix;
+        int m = l + r >> 1;
+        if(i <= m) {
+            nd[ix].l = update(nd[prev].l, i, l, m, v);
+            nd[ix].r = nd[prev].r;
+        }
+        else {
+            nd[ix].l = nd[prev].l;
+            nd[ix].r = update(nd[prev].r, i, m + 1, r, v);
+        }
+        return ix;
+    }
+    ll query(int prev, int ix, int nl, int nr, int l, int r) {
+        if(nl > r || nr < l) return 0;
+        if(nl >= l && nr <= r) return nd[ix].v - nd[prev].v;
+        int m = nl + nr >> 1;
+        return query(nd[prev].l, nd[ix].l, nl, m, l, r) + query(nd[prev].r, nd[ix].r, m + 1, nr, l, r);
+    }
+    int kth(int i1, int i2, int l, int r, int k) {
+        while (l ^ r) {
+            int cnt = nd[nd[i2].l].v - nd[nd[i1].l].v;
+            int m = l + r >> 1;
+            if (k <= cnt) r = m, i1 = nd[i1].l, i2 = nd[i2].l;
+            else l = m + 1, k -= cnt, i1 = nd[i1].r, i2 = nd[i2].r;
+        }
+        return l;
+    }
 }
 
-int upd(int prv, int i, int l, int r, ll v){
-	int ix = ++t;
-	val[ix] = val[prv] + v;
-	if(l == r) return ix;
-
-	int m = (l + r) >> 1;
-	if(i <= m) {
-		lc[ix] = upd(lc[prv], i, l, m, v);
-		rc[ix] = rc[prv];
-	}
-	else {
-		lc[ix] = lc[prv];
-		rc[ix] = upd(rc[prv], i, m + 1, r, v);
-	}
-	return ix;
-}
-
-// modify this function
-// idx 1개 기준 구간합
-ll query(int i1, int nl, int nr, int l, int r) {
-	if(nl > r || nr < l) return 0;
-	if(nl >= l && nr <= r) return val[i1];
-	int m = (nl + nr) >> 1;
-	return query(lc[i1], nl, m, l, r) + query(rc[i1], m + 1, nr, l, r);
-}
-
-// idx 2개 기준 (i2 seg에서 i1 seg를 뺀 상태에서) k번째 원소 구하기
-int kth(int i1, int i2, int l, int r, int k) {
-	while (l ^ r) {
-		ll cnt = val[lc[i2]] - val[lc[i1]];
-		int m = (l + r) >> 1;
-		if (k <= cnt) r = m, i1 = lc[i1], i2 = lc[i2];
-		else l = m + 1, k -= cnt, i1 = rc[i1], i2 = rc[i2];
-	}
-	return l;
-}
+pst::root[0] = pst::init(0, n);
